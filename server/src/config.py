cc6e331d63eff942d3fa7b7a332a75a74d132e8f
comcon1 <comconadin@gmail.com>
2015-09-12 13:52:13 +0300

    adding parametrs importing. package->dqpackage

--- server/src/config.py
+++ server/src/config.py
@@ -8,9 +8,24 @@ Created on Wed Aug  5 21:42:09 2015
 import os.path
 import sys
 
-sys.path.append('../../package')
+sys.path.append('../../')
 
 DQSPRGNAME = 'Rat Wheel Analyzer'
 DQSVERSION = '0.1'
 DQSROOTDIR = os.path.join(os.path.dirname(__file__), \
     '../../testsuite/testserver/')
+
+from iniparse.config import BasicConfig
+
+n = BasicConfig()
+f = open(os.path.join(DQSROOTDIR,'parameters.conf'))
+n._readfp(f)
+f.close()
+params = n
+
+import dqpackage
+
+dqpackage.params = n
+
+import dqpackage.libdaq as dq
+import dqpackage.libDQimage as qi
