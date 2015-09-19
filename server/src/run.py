7c4d78bee2724096b73bc9e4e5e689b1146c2225
Unknown <comcon1@localhost>
2015-09-19 18:46:08 +0300

    Add authentication options

--- server/src/run.py
+++ server/src/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import cherrypy
+import cherrypy.lib.auth_digest
 import os.path
 from config import *
 
@@ -13,7 +14,17 @@ root.ratstat = RatStatistics()
 
 print __file__
 
+
 curconf = os.path.join(DQSROOTDIR, 'server.conf')
+cherrypy.config.update(curconf)
+
+get_ha1 = cherrypy.lib.auth_digest.get_ha1_file_htdigest(\
+	os.path.join(DQSROOTDIR, 'htdigest') )
+
+print get_ha1
 
 if __name__ == '__main__':
-    cherrypy.quickstart(root, config=curconf)
+    root_app = cherrypy.tree.mount(root, "/", curconf)
+    root_app.config['/']['tools.auth_digest.get_ha1'] = get_ha1
+    cherrypy.engine.start()
+    cherrypy.engine.block() 
